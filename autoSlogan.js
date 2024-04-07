5d0fa3ff521ba958d3c8c821c1c065f37f75732f
Lucasgood <lucasgoodcambrai@gmail.com>
2024-04-07 20:38:12 +0200

    Title + Slogan + latency/RR

--- autoSlogan.js
+++ autoSlogan.js
@@ -1,8 +1,25 @@
 const Slogan = [
+    "Pour un solde bien gras,<br>Votez le rat, Votez Lucas !", // 2022
+    "Pour un compte bien rempli,<br>Votez gigi !",
+    "Pour un compte rond,<br>Votez le champignon !",
+    "Pour une caisse bien comptée,<br>c'est Girolle qu'il faut voter !",
+    "Pour un trésorier qui sais négocier,<br>c'est Girolle qu'il faut voter !",
+    "Pour ne pas que la caisse degringolle,<br>Votez Girolle !",
+    "Pour un comité épique,<br>Ajoutez Girolle à l'équipe !",
+    "Pour toujours plus de thunes,<br>Mettez Girolle dans l'urne !",
+
+    "Parce qu'il est tout mimi,<br>Votez Gigi !", // 2023
+    "Fais pas ta kaïra,<br>Vote pour Lucas !",
+    "Fais pas le mariole,<br>Vote pour Girolle",
+    "Pour un trésorier qui donne tout,<br>Votez Girolle, votre meilleur atout !",
+
+    //New OC
+    "Pour une année sans chichi,<br>Votez pour Gigi !",
     "Pour un compte qui s'envole,<br>Votez Girolle !",
     "Pour pas que la caisse dégringole,<br>Votez Girolle !",
-    "Pour une ambiance qui folle,<br>Votez Girolle !",
-    "d'autre slogan soonTM"
+    "Pour une ambiance folle,<br>Votez Girolle !",
+
+    "Pour que ton alcool reste moins cher que le gazole, Vote Girolle !", //Merci Garf
 ]
 
 function changeSlogan() {
